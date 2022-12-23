4bdf2eecd1a8b2010180fbb4729469188de6c5de
Inna Belkina <inna0311@inbox.ru>
2022-12-23 16:38:30 +0600

    Вторая программа

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    Console.WriteLine("Третья цифра " + number.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
